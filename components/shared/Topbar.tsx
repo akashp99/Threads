cc62e7d474ba6ab3b6480805da5527415132514a
Akash Pardeshi <pardeshiakash99@gmail.com>
2023-08-26 12:32:29 +0530

    topbar, leftbar, rightbar,bottombar added

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,13 +1,14 @@
 import { SignedIn, SignOutButton, OrganizationSwitcher } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { dark } from "@clerk/themes";
 
 function Topbar() {
     return (
     <nav className="topbar">
         <Link href="/" className="flex items-center gap-4">
             <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
-            <p className="text-heading3-bold text-light-1 max-xs:hidden">AKash</p>
+            <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
         </Link>
 
         <div className="flex items-center gap-1">
@@ -22,6 +23,7 @@ function Topbar() {
         </div>
         <OrganizationSwitcher
             appearance={{
+                baseTheme: dark,
                 elements: {
                     organizationSwitcherTrigger: "py-2 px-4"
                 }
@@ -32,4 +34,4 @@ function Topbar() {
     )
 }
 
-export default Topbar;
+export default Topbar;                      
